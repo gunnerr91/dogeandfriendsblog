57f9c23aa1ba53da02b4ab3ab0f8ac58378a2b67
gunnerr91 <blissvortex@hotmail.com>
2021-02-21 10:05:23 +0000

    simulating server

--- src/Home.js
+++ src/Home.js
@@ -2,27 +2,35 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([]);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:8080/blogs")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setBlogs(data);
-      });
+    setTimeout(() => {
+      fetch("http://localhost:8080/blogs")
+        .then((res) => res.json())
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 2000);
   }, []);
 
   return (
     <div className="home">
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === "doge")}
-        title="doge blogs"
-      />
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === "tartel")}
-        title="tartle blogs"
-      />
+      {isPending && <div>loading...</div>}
+      {blogs && (
+        <>
+          <BlogList
+            blogs={blogs.filter((blog) => blog.author === "doge")}
+            title="doge blogs"
+          />
+          <BlogList
+            blogs={blogs.filter((blog) => blog.author === "tartel")}
+            title="tartle blogs"
+          />
+        </>
+      )}
     </div>
   );
 };
