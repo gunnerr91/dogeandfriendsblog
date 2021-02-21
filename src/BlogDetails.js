f85ecf2be6d81ce705dfb39060e5b5c888182dae
gunnerr91 <blissvortex@hotmail.com>
2021-02-21 15:16:36 +0000

    added delete functionality

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 import "./blogdetails.css";
 
@@ -7,6 +7,15 @@ const BlogDetails = () => {
   const { data: blog, error, isPending } = useFetch(
     "http://localhost:8080/blogs/" + id
   );
+  const history = useHistory();
+  const handleClick = (e) => {
+    fetch("http://localhost:8080/blogs/" + id, {
+      method: "DELETE"
+    }).then(() => {
+      history.push("/");
+    });
+  };
+
   return (
     <div className="blog-details">
       {isPending && <div>loading....</div>}
@@ -16,6 +25,7 @@ const BlogDetails = () => {
           <h3>{blog.title}</h3>
           <i>written by {blog.author}</i>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>delete</button>
         </article>
       )}
     </div>
