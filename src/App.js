6ef8f63d0f579c0ebd2efb6726a11da0d22657ee
gunnerr91 <blissvortex@hotmail.com>
2021-02-21 13:06:14 +0000

    added react router package

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import NavBar from "./Navbar";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <div className="content">
-        <Home />
-      </div>
+      <BrowserRouter>
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Route path="/" component={Home} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
